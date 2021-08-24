292d984c09de34a2a122fd1f302caab344484a2f
dangkhang1811 <dangkhang1811@gmail.com>
2021-08-24 20:57:38 +0700

    first commit

---
+++ amazon-clone-two-master/firebase.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD2pPSdkw8sy_c4zZTTnzw98SFHO2cWBII",
+  authDomain: "fir-303b4.firebaseapp.com",
+  projectId: "fir-303b4",
+  storageBucket: "fir-303b4.appspot.com",
+  messagingSenderId: "607591175941",
+  appId: "1:607591175941:web:fb2b9cf90c166e5fde896c",
+  measurementId: "G-CL6RTBT7HD"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var db = firebase.firestore();
+
